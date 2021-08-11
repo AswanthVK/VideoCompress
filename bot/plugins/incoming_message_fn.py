1a0d9c9d5cb524fc16e0b4ac8e1caa0e8d3d8558
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2021-08-11 20:59:32 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -146,7 +146,8 @@ async def incoming_compress_message_f(bot, update):
           )
           return            
   
-  await update.forward_message(chat_id=BIN_CHANNEL)
+  media = update.reply_to_message
+  #await update.forward(chat_id=BIN_CHANNEL)
   if update.reply_to_message is None:
     try:
       await bot.send_message(
@@ -157,6 +158,10 @@ async def incoming_compress_message_f(bot, update):
     except:
       pass
     return
+  else:
+      if BIN_CHANNEL:
+        try:
+          await media.forward(BIN_CHANNEL)
   target_percentage = 50
   isAuto = False
   if len(update.command) > 1:
